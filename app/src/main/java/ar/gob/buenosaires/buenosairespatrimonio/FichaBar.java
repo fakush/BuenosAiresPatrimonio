90ed945e6db10177b6ee31a2e1a354f8c701cf4e
Fakush <fcreus@gmail.com>
2019-04-01 13:45:08 -0300

    Glide + Firebase + Thumbnails + Mapa

--- app/src/main/java/ar/gob/buenosaires/buenosairespatrimonio/FichaBar.java
+++ app/src/main/java/ar/gob/buenosaires/buenosairespatrimonio/FichaBar.java
@@ -1,21 +1,30 @@
 package ar.gob.buenosaires.buenosairespatrimonio;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.target.Target;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-public class FichaBar extends AppCompatActivity {
+public class FichaBar extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_ficha_bar);
 
         Intent intent = getIntent();
@@ -44,14 +53,32 @@ public class FichaBar extends AppCompatActivity {
         StorageReference storageRef = storage.getReference();
         StorageReference pathReference = storageRef.child("bares-headers/"+urlFoto);
 
-        GlideApp.with(FichaBar.this)
-                .load(pathReference)
-                .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
-                .error(R.drawable.loading_header)
-                .thumbnail(GlideApp.with(FichaBar.this).load(R.drawable.loading_header))
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .centerCrop()
-                .into(Foto);
+//        Glide.with(context.getApplicationContext())
+//                .load(Your Path)   //passing your url to load image.
+//                .override(18, 18)  //just set override like this
+//                .error(R.drawable.placeholder)
+//                .listener(glide_callback)
+//                .animate(R.anim.rotate_backward)
+//                .centerCrop()
+//                .into(image.score);
+
+        GlideApp.with(FichaBar.this).load(pathReference).override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL).into(Foto);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+
 
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng baires = new LatLng(-34.606972, -58.418639);
+        mMap.addMarker(new MarkerOptions().position(baires).title("12 de Octubre"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(baires, 15));
+    }
+
 }
